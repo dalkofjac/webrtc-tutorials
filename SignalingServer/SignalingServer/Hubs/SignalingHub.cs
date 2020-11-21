b9983012dab7c138c4787a80cb2e3e86f33be0be
dalkofjac <dadolg22@gmail.com>
2020-11-21 18:52:49 +0100

    Added signaling server

---
+++ SignalingServer/SignalingServer/Hubs/SignalingHub.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SignalingServer.Hubs
+{
+    // [Authorize]
+    public class SignalingHub : Hub
+    {
+        public Dictionary<string, List<string>> ConnectedClients = new Dictionary<string, List<string>>();
+
+        public Task SendMessage(object message, string roomName)
+        {
+            EmitLog("Client said: " + message, roomName);
+
+            return Clients.OthersInGroup(roomName).SendAsync("message", message);
+        }
+
+        public Task CreateOrJoinRoom(string roomName)
+        {
+            EmitLog("Received request to create or join room " + roomName + " from a client " + Context.ConnectionId, roomName);
+
+            if (!ConnectedClients.ContainsKey(roomName))
+            {
+                ConnectedClients.Add(roomName, new List<string>());
+            }
+
+            if (!ConnectedClients[roomName].Contains(Context.ConnectionId))
+            {
+                ConnectedClients[roomName].Add(Context.ConnectionId);
+            }
+
+            EmitJoinRoom(roomName);
+            
+            var numberOfClients = ConnectedClients[roomName].Count;
+
+            if (numberOfClients == 1)
+            {
+                EmitCreated(roomName);
+                EmitLog("Client "+ Context.ConnectionId + " created the room " + roomName, roomName);
+            }
+            else
+            {
+                EmitJoined(roomName);
+                EmitLog("Client " + Context.ConnectionId + " joined the room " + roomName, roomName);
+            }
+
+            EmitLog("Room " + roomName + " now has " + numberOfClients + " client(s)", roomName);
+
+            return Task.Run(() => { return; });
+        }
+
+        public Task LeaveRoom(string roomName)
+        {
+            EmitLog("Received request to leave the room " + roomName + " from a client " + Context.ConnectionId, roomName);
+
+            if (ConnectedClients.ContainsKey(roomName) && ConnectedClients[roomName].Contains(Context.ConnectionId))
+            {
+                ConnectedClients[roomName].Remove(Context.ConnectionId);
+                EmitLog("Client " + Context.ConnectionId + " left the room " + roomName, roomName);
+
+                if (ConnectedClients[roomName].Count == 0)
+                {
+                    ConnectedClients.Remove(roomName);
+                    EmitLog("Room " + roomName + " is now empty - resetting its state.", roomName);
+                }
+            }
+
+            return Groups.RemoveFromGroupAsync(Context.ConnectionId, roomName);
+        }
+
+        private Task EmitJoinRoom(string roomName)
+        {
+            return Groups.AddToGroupAsync(Context.ConnectionId, roomName);
+        }
+
+        private Task EmitCreated(string roomName)
+        {
+            return Clients.Caller.SendAsync("created", roomName);
+        }
+
+        private Task EmitJoined(string roomName)
+        {
+            return Clients.Group(roomName).SendAsync("joined", roomName);
+        }
+
+        private Task EmitLog(string message, string roomName)
+        {
+            return Clients.Group(roomName).SendAsync("log", message);
+        }
+    }
+}
---
+++ SignalingServer/SignalingServer/Startup.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SignalingServer.Hubs;
+
+namespace SignalingServer
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDefaultFiles();
+            
+            app.UseStaticFiles();
+            
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHub<SignalingHub>("/hubs/signaling");
+            });
+        }
+    }
+}
