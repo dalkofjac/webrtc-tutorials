1c945d3ef1ce943e7a8e9dcd6c791f6546db0d46
Vladimir Shirmanov <vladimir.shirmanov@gmail.com>
2023-11-16 23:47:16 +0100

    Update environment.ts

--- WebRTCWebApp/src/environments/environment.ts
+++ WebRTCWebApp/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  signalingServerUrl: 'http://localhost:5000/hubs',
+  signalingServerUrl: 'https://az204webapi.azurewebsites.net/hubs',
   iceServers: [
     {urls: 'stun:stun.1.google.com:19302'},
     {urls: 'stun:stun1.l.google.com:19302'}
